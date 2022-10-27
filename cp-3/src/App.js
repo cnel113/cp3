123ee0dd6def557d0bb21c286525e622e6937418
Carly Nelson <chermansen723@gmail.com>
2022-10-27 02:02:45 +0000

    starting App.js

---
+++ cp-3/src/App.js
@@ -0,0 +1,84 @@
+import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    
+    class colorForm extends React.Component {
+        constructor(props) { //ask what props is
+            super(props); //ask what super is
+            this.hue = "Random";
+            this.number = 1;
+            this.light = false;
+            this.dark = false;
+            this.hexColor = "#E2A2CC";
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+  document.getElementById("submitButton").addEventListener("click", function(event) {
+    event.preventDefault();
+    let colorField = document.getElementById('hue-selector');
+    let selectedHue = colorField.options[colorField.selectedIndex].value;
+    let amountField = document.getElementById('amount-selector');
+    let numColors = amountField.options[amountField.selectedIndex].value;
+    let light = document.getElementById('light').checked;
+    let dark = document.getElementById('dark').checked;
+    
+    const box = document.getElementById('colorResults');
+    box.innerHTML = ""; 
+    
+    let url ="https://x-colors.herokuapp.com/api/random";
+    
+    if (selectedHue != "random") {
+        url += "/" + selectedHue;
+    }
+    else {
+        url += "/all";
+    }
+    url += "?number=" + numColors;
+    
+    if (light === true && dark === false) {
+        url += "&type=light";
+    }
+    else if (light === false && dark === true) {
+        url += "&type=dark";
+    }
+    
+    fetch(url) 
+    .then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        console.log(json);
+            for (let i = 0; i < numColors; i++){
+                let color = ""
+                if (numColors === "1") {
+                    color = json.hex;
+                }
+                else {
+                    color = json[i].hex;
+                }
+                let chip = document.createElement('div');
+                chip.classList.add('color-chip');
+                let el = document.createElement('div');
+                el.classList.add('color-block');
+                el.style.backgroundColor = color;
+                chip.appendChild(el);
+                let text = document.createElement('p');
+                text.innerHTML = color;
+                chip.appendChild(text);
+                box.appendChild(chip);
+            }
+        });
+            
+      
+  });
+}
+
+export default App;
